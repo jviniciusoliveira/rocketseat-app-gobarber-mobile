bb61594ab357d5b0bdbb409a558c3fda351ba0a5
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-18 13:42:42 -0300

    feat: add Input and Button components

---
+++ GoBarber/src/components/Input/index.js
@@ -0,0 +1,26 @@
+import React, {forwardRef} from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
+
+import {Container, TextInput} from './styles';
+
+const Input = forwardRef(({style, icon, ...rest}, ref) => {
+  return (
+    <Container style={style}>
+      {icon && <Icon name={icon} size={20} color="rgba(255, 255, 255, 0.6)" />}
+      <TextInput {...rest} />
+    </Container>
+  );
+});
+
+Input.propTypes = {
+  icon: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+};
+
+Input.defaultProps = {
+  icon: null,
+  style: {},
+};
+
+export default Input;
--- GoBarber/src/pages/SignIn/index.js
+++ GoBarber/src/pages/SignIn/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
+
+const ref = React.createRef();
 
 const SignIn = () => {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="Exemplo Input"
+        ref={ref}
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 };
