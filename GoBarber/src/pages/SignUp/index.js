2b662aec29bbf1bf0c4c16a73ad2aaf0c6d4cddb
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-18 12:40:15 -0300

    feat: add routes (SignIn SignUp)

---
+++ GoBarber/src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import Background from '~/components/Background';
+
+const SignUp = () => {
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  );
+};
+
+export default SignUp;
