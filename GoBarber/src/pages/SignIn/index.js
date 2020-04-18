998d292e4c8c57e14ef3b74f871a83299a7f5be4
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-18 14:08:28 -0300

    feat: add component SignIn styles

--- GoBarber/src/pages/SignIn/index.js
+++ GoBarber/src/pages/SignIn/index.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 const ref = React.createRef();
 
 const SignIn = () => {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{marginTop: 30}}
-        icon="call"
-        placeholder="Exemplo Input"
-        ref={ref}
-      />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
 
-      <Button>Entrar</Button>
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 };
